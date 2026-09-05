581bd6bf907143887d07f035fc0a1a6e0a97f0e0
zengxiangyi <zengxiangyi@126.com>
2026-09-05 22:03:30 +0800

    更新项目

--- backend/src/main/java/com/baogang/info/repository/WorkflowRepository.java
+++ backend/src/main/java/com/baogang/info/repository/WorkflowRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -33,4 +34,8 @@ public interface WorkflowRepository extends JpaRepository<Workflow, Long> {
     List<Workflow> findByFlowGraph(String flowGraph);
 
     Page<Workflow> findBySender(String sender, Pageable pageable);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("update Workflow w set w.state = :state where w.code = :code")
+    int updateState(@Param("state") String state,@Param("code") String code);
 }
